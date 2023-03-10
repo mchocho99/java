b62121381da893a8da889e47c5c4632bf4bf2dd1
mchocho99 <martinchocho99@gmail.com>
2023-03-10 17:22:09 -0300

    Move Main.java to a package

--- learning-java/src/com/amigoscode/Main.java
+++ learning-java/src/com/amigoscode/Main.java
@@ -1,5 +1,7 @@
+package com.amigoscode;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("a ver ahora");
+        System.out.println("Hello world!");
     }
 }
